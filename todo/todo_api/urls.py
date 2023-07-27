604e349b3560e7dca74b1eae7a1ae7d6a5124a36
Ethan-LN <et.l.nicholas@gmail.com>
2023-07-27 18:58:00 +1000

    use ID to find the details of API view

--- todo/todo_api/urls.py
+++ todo/todo_api/urls.py
@@ -1,10 +1,11 @@
-# todo/todo_api/urls.py : API urls.py
+# todo/api/urls.py : API urls.py
 from django.urls import path, include
 from .views import (
     TodoListApiView,
+    TodoDetailApiView
 )
 
 urlpatterns = [
     path('api', TodoListApiView.as_view()),
-
+    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
 ]
